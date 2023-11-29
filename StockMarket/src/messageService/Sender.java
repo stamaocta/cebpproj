206c1dad28b4478fe5b53b0294fb9731a3a6a0a0
Nicoleta Voinea <nicoletavoinea17@yahoo.com>
2023-11-29 12:49:00 +0200

    Added locks for sender class

--- StockMarket/src/messageService/Sender.java
+++ StockMarket/src/messageService/Sender.java
@@ -7,19 +7,24 @@ import com.rabbitmq.client.ConnectionFactory;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Sender {
 
     private static String queue_name;
     private ConnectionFactory factory;
+    private static Lock lock=new ReentrantLock();
 
     public Sender(String queue_name){
+        lock.lock();
         this.queue_name=queue_name;
         factory = new ConnectionFactory();
         factory.setHost("localhost");
     }
 
     public void sendMessage(String message) throws IOException, TimeoutException {
+
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
                 channel.queueDeclare(queue_name, false, false, false, null);
@@ -27,6 +32,7 @@ public class Sender {
                 System.out.println("[ " + queue_name +" ] Sent '" + message + "'");
 
             }
+        lock.unlock();
     }
 
 
